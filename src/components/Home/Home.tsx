98d303ad1457240ecfff7ded71c18bc753603eb9
Dante <guchoo923@gmail.com>
2024-10-29 16:00:03 +0200

    style home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,30 +1,37 @@
 import homeImg from "../../assets/homeImg.png";
 import homeForeground from "../../assets/homeForeground.png";
+import "./home.css";
 
-function Home() {
+export default function Home() {
   return (
     <div>
-      <div>
-        <img src={homeImg} alt="Hero" />
+      <div className="home-outer-wrap">
+        <img 
+          src={homeImg} 
+          className="home-img" 
+          alt="Home Image" 
+        />
+
         <img
           src={homeForeground}
           alt="Foreground"
+          className="home-foreground-img"
         />
-        <div>
-          <div>
+
+        <div className="home-content-wrap">
+          <div className="home-text-wrap">
             <h1>Build a self care routine suitable for you</h1>
             <p>
               Take out test to get a personalised self care routine based on
               your needs
             </p>
           </div>
-          <div>
-            <button>Start the quiz</button>
+
+          <div className="home-button-wrap">
+            <button className="home-button">Start the quiz</button>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
-export default Home;
